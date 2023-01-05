461bbb11a9003851b9f4e252cfc5d86a519d109f
qinjialei24 <qinjialei@foxmail.com>
2023-01-05 21:35:28 +0800

    feat: render component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp, h } from 'vue'
+console.log("🚀 ~ file: main.js:2 ~ createApp", createApp)
+
+
 
 createApp({
     render() {
+        console.log('111');
         return h('div', 'my-vite')
     }
 }).mount("#app")
